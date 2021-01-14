e3f6971cfd4601ddf752280ec25295470017f6e6
samuelmarina <samuellmarina@gmail.com>
2021-01-13 23:49:03 -0400

    Construir drawer menu con data dummy

--- src/components/SideBar/Sidebar.js
+++ src/components/SideBar/Sidebar.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import { Drawer, ListItem, ListItemText, List } from "@material-ui/core";
 
-function Sidebar(props) {
+/** Componente que representa la barra lateral de navegación */
+function Sidebar({ items }) {
     return (
-        <div>
-            
-        </div>
+        <Drawer
+            anchor="right"
+            variant="permanent"
+        >
+            <List>
+                {
+                    items.map((text, index) => {
+                        return (
+                            <ListItem button key={index}>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        </Drawer>
     );
+}
+
+Sidebar.propTypes ={
+    items: PropTypes.array
 }
 
 export default Sidebar;
