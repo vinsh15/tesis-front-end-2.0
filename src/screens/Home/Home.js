b728c0d2f4f5a9cab432f8ef9eb8ac24e0c5fbd6
Maria Gabriela <cafarelli172006@gmail.com>
2021-02-06 18:09:54 -0400

    changes

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar';
+import React, { useState } from 'react';
+import Sidebar from '../../components/SideBar/Sidebar';
 
 const dummydata = ["Grafo 1", "Grafo 2", "Grafo 3"];
 
@@ -8,10 +8,17 @@ const dummydata = ["Grafo 1", "Grafo 2", "Grafo 3"];
  */
 function Home() {
     const [drawerItems, setDrawerItems] = React.useState(dummydata);
+    const [usuario, setUsuario] = useState(false);
+  
+    async function prueba() {
+      console.log("usuario true");
+      setUsuario(true);
+    }
+  
 
     return (
         <>
-            <Sidebar items={drawerItems}/>
+            <Sidebar items={drawerItems} user="false" />
             <h1>Home Page</h1>
         </>
     );
