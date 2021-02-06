29d17a529958696daf27bc0d048422dd364e0912
samuelmarina <samuellmarina@gmail.com>
2021-02-06 14:40:54 -0400

    Agregar funciones de inicialización y restauración de usuario

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./screens/Home/Home";
 import './App.css';
+
+import AuthContext from "./auth/context/context";
 
 /** Componente que contiene todas las rutas
  *  de navegación en la aplicación
  */
 function App() {
+  const [user, setUser] = useState();
+
+  /**
+   * Restaurar usuario si ya se ha iniciado sesión
+   */
+  const restoreUser = async () => {
+    const user = await localStorage.getItem('user');
+    if(user) setUser(JSON.parse(user));
+  }
+
+  useEffect(() => {
+    restoreUser();
+  }, [])
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
