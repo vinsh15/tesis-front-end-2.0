21834d478da311aaf92ff144a6711eb059280fda
samuelmarina <samuellmarina@gmail.com>
2021-03-01 12:59:13 -0400

    Crear funciones para api de arquitecturas

---
+++ src/api/architecture/architecture.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+/**
+ * Agregar una nueva arquitectura a la base de datos del usuario
+ * @param {FormData} formData objeto form-data con la información de la arquitectura
+ */
+const postArchitecture = async (formData) => {
+    try {
+        const response = await axios.post("/arquitecturas/", formData,
+        {
+            headers: {'Content-Type': 'multipart/form-data' }
+        });
+        return response.data;
+    }
+    catch(error){
+        return error;
+    }
+}
+
+export {
+    postArchitecture,
+}
