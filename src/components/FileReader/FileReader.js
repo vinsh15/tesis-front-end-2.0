b42f6fa2ef5db8871cd359dd3b5ba149b4757cf6
samuelmarina <samuellmarina@gmail.com>
2021-03-04 21:24:50 -0400

    Refactorizacion de FileReader

--- src/components/FileReader/FileReader.js
+++ src/components/FileReader/FileReader.js
@@ -1,55 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./FileReader.css";
 import "react-dropzone-uploader/dist/styles.css";
 
-import Swal from "sweetalert2";
-
-import { makeStyles } from "@material-ui/core/styles";
-import { postArchitecture } from "../../api/architecture/architecture";
+import AppContext from "../../auth/context/context";
 import Dropzone from "react-dropzone-uploader";
-import Modal from "@material-ui/core/Modal";
-import TextField from "@material-ui/core/TextField";
-
+import { makeStyles } from "@material-ui/core/styles";
 import Loader from "../Loader/Loader";
-
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    top: "20%",
-    left: "20%",
-    width: "60%",
-    minHeight: 300,
-    maxHeight: 600,
-    overflow: 'auto',
-    border: "none",
-    borderRadius: 5,
-    background: "var(--background)",
-    boxShadow: theme.shadows[5],
-    padding: "32px 24px 20px",
-  },
-  h1: {
-    color: "var(--primaryDark)",
-    margin: "auto",
-    marginBottom: 35,
-    textAlign: "center",
-    fontFamily: "var(font-family-content)",
-  },
-  validation: {
-    color: "var(--error)",
-    fontSize: 13,
-    margin: 5
-  }
-}));
+import Modal from "@material-ui/core/Modal";
+import { postArchitecture } from "../../api/architecture/architecture";
+import Swal from "sweetalert2";
+import TextField from "@material-ui/core/TextField";
 
 /** Componente que representa pop-up
  *  para añadir archivos
  */
-function FileReader(props) {
+const FileReader = ({
+  onClose,
+  open,
+  projectIndex,
+  type,
+}) => {
   const classes = useStyles();
-  const [name, setName] = React.useState("");
-  const [valid, setValid] = React.useState(true);
-  const [loader, setLoader] = React.useState(false);
+  const [name, setName] = useState("");
+  const [valid, setValid] = useState(true);
+  const [loader, setLoader] = useState(false);
+  const { user } = useContext(AppContext);
 
   const handleChangeStatus = ({ meta }, status) => {
     //console.log(status, meta)
@@ -71,9 +46,9 @@ function FileReader(props) {
    */
   const getFormData = (allFiles) => {
     const formData = new FormData();
-    formData.append('uid', props.uid);
+    formData.append('uid', user.uid);
     formData.append('name', name);
-    formData.append('index', props.index);
+    formData.append('index', projectIndex);
     allFiles.forEach(file => {
       addFile(file, formData);
     })
@@ -119,7 +94,7 @@ function FileReader(props) {
    */
   function swlSuccess() {
     setTimeout(setLoader(false), 3000);
-    props.onClose();
+    onClose();
     Swal.fire({
         title: '¡Arquitectura creada!',
         icon: 'success',
@@ -133,7 +108,7 @@ function FileReader(props) {
    */
   function swlError() {
     setTimeout(setLoader(false), 3000);
-    props.onClose();
+    onClose();
     Swal.fire({
       icon: 'error',
       title: '¡Hubo un error!',
@@ -148,13 +123,13 @@ function FileReader(props) {
       <Modal
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
-        open={props.open}
-        onClose={props.onClose}
+        open={open}
+        onClose={onClose}
       >
         <div className={classes.paper}>
         {loader ? <Loader /> : 
         <>
-          <h2 className={classes.h1}>Agregar {props.type}</h2>
+          <h2 className={classes.h1}>Agregar {type}</h2>
           <TextField
             required
             id="outlined-basic"
@@ -186,5 +161,35 @@ function FileReader(props) {
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    top: "20%",
+    left: "20%",
+    width: "60%",
+    minHeight: 300,
+    maxHeight: 600,
+    overflow: 'auto',
+    border: "none",
+    borderRadius: 5,
+    background: "var(--background)",
+    boxShadow: theme.shadows[5],
+    padding: "32px 24px 20px",
+  },
+  h1: {
+    color: "var(--primaryDark)",
+    margin: "auto",
+    marginBottom: 35,
+    textAlign: "center",
+    fontFamily: "var(font-family-content)",
+  },
+  validation: {
+    color: "var(--error)",
+    fontSize: 13,
+    margin: 5
+  }
+}));
+
 
 export default FileReader;
