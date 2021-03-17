cbf43873bc3d8f348434dbf90d507fbcee6b066a
samuelmarina <samuellmarina@gmail.com>
2021-03-16 20:32:22 -0400

    Agregar función delete a api de versiones

--- src/api/versions/versions.js
+++ src/api/versions/versions.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import jwt from "jwt-encode";
+
+const url = "/version/";
 
 /**
  * Agregar una nueva versión a la base de datos del usuario
@@ -6,7 +9,7 @@ import axios from "axios";
  */
 const postVersion = async (formData) => {
     try {
-        const response = await axios.post("/version/", formData,
+        const response = await axios.post(url, formData,
         {
             headers: {'Content-Type': 'multipart/form-data' }
         });
@@ -17,6 +20,37 @@ const postVersion = async (formData) => {
     }
 }
 
+/**
+ * Eliminar una versión de la base de datos
+ * @param {JSON} user objeto con la información del usuario
+ * @param {Integer} projectIndex índice del proyecto
+ * @param {Integer} arcIndex índice de la arquitectura
+ * @param {Integer} verIndex índice de la versión
+ * @returns versiones de una arquitectura de un proyecto
+ *  del usuario o error
+ */
+const deleteVersion = async (user, projectIndex, arcIndex, verIndex) => {
+    try {
+        const userInfo = {
+            user_id: user.id,
+            project_index: projectIndex,
+            arc_index: arcIndex,
+            ver_index: verIndex
+        };
+        const token = jwt(userInfo, 'secret');
+        const response = await axios.delete(url, {
+            data: {
+                token: token
+            }
+        });
+        return response.data;
+
+    } catch (error) {
+        return "Error";
+    }
+}
+
 export {
+    deleteVersion,
     postVersion,
 }
