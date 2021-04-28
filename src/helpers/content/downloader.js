2dfbb57c5da03a3bab5ae7ad26c0f7a6c6e4db50
samuelmarina <samuellmarina@gmail.com>
2021-04-27 21:06:27 -0400

    Crear helper para content

---
+++ src/helpers/content/downloader.js
@@ -0,0 +1,27 @@
+import { saveAs } from "@progress/kendo-file-saver";
+
+const downloadGraph = (type, cy, name) => {
+    const options = {
+        bg: "#fff"
+    };
+    let uri;
+    let fileName = name;
+    switch (type) {
+        case "png":
+            uri = cy.png(options);
+            fileName += ".png";
+            break;
+        case "jpg":
+            uri = cy.jpg(options);
+            fileName += ".jpg";
+            break;
+        default:
+            break;
+    }
+
+    saveAs(uri, fileName);
+}
+
+export default {
+    downloadGraph,
+}
