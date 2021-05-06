dd8984f79487e0a128f91308a02f4fc8ac899ab3
samuelmarina <samuellmarina@gmail.com>
2021-05-05 22:00:10 -0400

    Agregar manejador de click cell

--- src/helpers/nodes/nodes.js
+++ src/helpers/nodes/nodes.js
@@ -10,7 +10,20 @@ const removeNode = (id, selectedNodes, setSelectedNodes) => {
     setSelectedNodes(temp);
 }
 
+const manageCellClick = (nodeArray, selectedNodes, setSelectedNodes, setSelectionModel) => {
+    const tempSet = new Set(nodeArray);
+    const temp = new Set();
+    selectedNodes.forEach(node => {
+        if(tempSet.has(node)){
+            temp.add(node);
+        }
+    });
+    setSelectedNodes(temp);
+    setSelectionModel([...temp]);
+}
+
 export default {
     addNode,
+    manageCellClick,
     removeNode,
 }
