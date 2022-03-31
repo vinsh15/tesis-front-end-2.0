ca3fc7a443f9d37396633665ffccd67a0bcf1de3
Vincent Perez <vinsh15@gmail.com>
2022-03-31 19:23:11 -0400

    validador de existencia packageMapping

--- src/helpers/nodes/nodes.js
+++ src/helpers/nodes/nodes.js
@@ -236,7 +236,7 @@ const getRelationData = (selectedProject) => {
       coupling: edge.metrics.coupling.value,
       instability: edge.metrics.instability.value,
       nameResemblance: edge.metrics.nameResemblance.value,
-      packageMapping: edge.metrics.packageMapping.value,
+      packageMapping: edge.metrics.packageMapping?.value,
 
     };
   })
