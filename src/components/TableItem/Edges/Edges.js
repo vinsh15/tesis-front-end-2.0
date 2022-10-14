1454c1853555074408787d8319bed9abe7ffaa81
cesareuseche <cesar@vaangroup.com>
2022-10-13 21:41:27 -0400

    hipotesis terminada cherry picking

--- src/components/TableItem/Edges/Edges.js
+++ src/components/TableItem/Edges/Edges.js
@@ -33,7 +33,7 @@ const EdgesTable = () => {
     { field: "nameRessemblance", headerName: "Semejanza de Nombre", width: 200 },
     { field: "packageMapping", headerName: "Mapeo de Paquetes", width: 200 },
     { field: "q", headerName: "Q", width: 250 },
-    { field: "answer", headerName: "Answer", width: 250 },
+    { field: "answer", headerName: "Candidato a Compuesto?", width: 250 },
 
   ];
 
@@ -41,14 +41,16 @@ const EdgesTable = () => {
   const [dms, setDms] = useState(15);
   const [nameResemblance, setNameResemblance] = useState(35);
   const [packageMapping, setPackageMapping] = useState(35);
-  const [umbralName, setUmbralName] = useState(0.35);
+  const [umbralName, setUmbralName] = useState(35);
   const [umbralCoupling, setUmbralCoupling] = useState(0.65);
-  const [umbral, setUmbral] = useState(0.5);
-  const [sum, setSum] = useState(dms, nameResemblance, packageMapping);
+  const [umbral, setUmbral] = useState(0.4);
+  const [sum, setSum] = useState(dms + nameResemblance + packageMapping);
+  let total = (dms + nameResemblance + packageMapping)
 
   // Calculate the sum total of all the input fields
   function calculateTotal() {
     setSum(dms + nameResemblance + packageMapping);
+    total = sum;
   }
 
   // Getting all the nodes and mapping through each item
@@ -77,14 +79,13 @@ const EdgesTable = () => {
           nodesDos[j].id === edgesDos[i].source &&
           nodesDos[j].incomompleteProperties
         ) {
-          console.log("PASO1")
+          
           flag1 = true;
           edgesDos[i].q = 0;
           edgesDos[i].answer = "No Aplica";
         }
         if (
           nodesDos[j].id === edgesDos[i].target && nodesDos[j].incomompleteProperties) {
-          console.log("PASO2")
           flag2 = true;
           edgesDos[i].q = 0;
           edgesDos[i].answer = "No Aplica";
@@ -94,13 +95,15 @@ const EdgesTable = () => {
         }
       }
       if (!flag1 && !flag2) {
-        console.log("PASO3")
 
-        if (edgesDos[i].coupling >= 0.6) {
-          console.log("PASO4")
+        console.log("partida " + edgesDos[i].source + "llegada " + edgesDos[i].target)
+        console.log("Nombre: " +  edgesDos[i].nameRessemblance + " Mapeo " + edgesDos[i].packageMapping)
+
+        if (edgesDos[i].coupling >= umbralCoupling) {
           dividen1 =
-            edgesDos[i].nameResemblance * nameResemblance;
+            edgesDos[i].nameRessemblance * nameResemblance;
         }
+
         dividen1 = dividen1 + edgesDos[i].packageMapping * packageMapping;
         dividen2 = edgesDos[i].dms * dms;
 
@@ -108,11 +111,14 @@ const EdgesTable = () => {
         edgesDos[i].q = q.toFixed(2);
 
         if (q >= umbral) {
-          console.log("PASO5")
-          edgesDos[i].answer = "SI";
+          edgesDos[i].answer = "Si";
         } else {
-          edgesDos[i].answer = "NO";
+          edgesDos[i].answer = "No";
         }
+      }else{
+        edgesDos[i].q = 'Imposible Calcular'
+        edgesDos[i].answer = 'Imposible Concluir'
+
       }
 
     }
@@ -127,106 +133,101 @@ const EdgesTable = () => {
   const handleClose = () => setOpen(false);
 
 
-
-
-
-
   return (
     <div style={{ height: "80vh", width: "100%" }}>
-    <div className="form-wrapper">
-        <form className="form-umbral">
+      <form className="form-styles">
+        <div className="input">
+          <div className="input-align">
+            <input
+              value={dms}
+              onChange={(e) => {setDms(+e.target.value) 
+                calculateTotal()}}
+              className="input-styles"
+              placeholder="ejm. 15"
+              name="dms"
+            />
+            <label className="input-label">Peso DMS</label>
+          </div>
+          <div className="input-align">
+            <input
+              className="input-styles"
+              placeholder="ejm. 35"
+              name="semejanza"
+              value={nameResemblance}
+              onChange={(e) => {setNameResemblance(+e.target.value)
+                                calculateTotal()}}
+            />
+            <label className="input-label">Peso Semejanza de Nombre</label>
+          </div>
+          <div className="input-align">
+            <input
+              className="input-styles"
+              placeholder="ejm. 35"
+              name="paquete"
+              value={packageMapping}
+              onChange={(e) => {setPackageMapping(+e.target.value)
+                                calculateTotal() }}
+            />
+            <label className="input-label">Peso Mapeo de Paquete</label>
+          </div>
           <div className="input-align-umbral">
             <input
               className="input-styles-umbral"
               placeholder="ejm. 0.45"
               name="umbral"
               value={umbralName}
-              type="number"
-              min="0"
-              max="1"
+              type="text"
               onChange={(e) => setUmbralName(e.target.value)}
             />
             <label className="input-label">Umbral Semejanza</label>
           </div>
-          <div className="btn-total">
-            <Button onClick={() => {
-              ManageMetrics(user, selectedProject, umbralName)
-            }
-            }>
-              Calcular Metricas
-            </Button>
+          <div className="input-align-umbral">
+            <input
+              className="input-styles-umbral"
+              placeholder="ejm. 0.65"
+              name="umbral"
+              value={umbralCoupling}
+              type="text"
+              onChange={(e) => setUmbralCoupling(e.target.value)}
+            />
+            <label className="input-label">Umbral Acoplamiento</label>
           </div>
-        </form>
-        <div className="divider-edges"></div>
-        <form className="form-styles">
-          <div className="input">
-            <div className="input-align">
-              <input
-                value={dms}
-                onChange={(e) => setDms(+e.target.value)}
-                className="input-styles"
-                placeholder="ejm. 15"
-                name="dms"
-              />
-              <label className="input-label">Peso DMS</label>
-            </div>
-            <div className="input-align">
-              <input
-                className="input-styles"
-                placeholder="ejm. 35"
-                name="semejanza"
-                value={nameResemblance}
-                onChange={(e) => setNameResemblance(+e.target.value)}
-              />
-              <label className="input-label">Peso Semejanza de Nombre</label>
-            </div>
-            <div className="input-align">
-              <input
-                className="input-styles"
-                placeholder="ejm. 35"
-                name="paquete"
-                value={packageMapping}
-                onChange={(e) => setPackageMapping(+e.target.value)}
-              />
-              <label className="input-label">Peso Mapeo de Paquete</label>
-            </div>
-            <div className="input-align-umbral">
-              <input
-                className="input-styles-umbral"
-                placeholder="ejm. 0.65"
-                name="umbral"
-                value={umbralCoupling}
-                type="number"
-                min="0"
-                max="1"
-                onChange={(e) => setUmbralCoupling(e.target.value)}
-              />
-              <label className="input-label">Umbral Acoplamiento</label>
-            </div>
-            <div className="input-align-umbral">
-              <input
-                className="input-styles-umbral"
-                placeholder="Umbral"
-                name="umbral"
-                value={umbral}
-                type="number"
-                min="0"
-                max="1"
-                onChange={(e) => setUmbral(e.target.value)}
-              />
-              <label className="input-label">Umbral Q</label>
-            </div>
+          <div className="input-align-umbral">
+            <input
+              className="input-styles-umbral"
+              placeholder="Umbral"
+              name="umbral"
+              value={umbral}
+              type="text"
+              onChange={(e) => setUmbral(e.target.value)}
+            />
+            <label className="input-label">Umbral Q</label>
           </div>
-          <div className="btn-total">
-            <Button onClick={calculateTotal} variant="contained">
-              Calcular
-            </Button>
-          </div>
-        </form >
-    </div>
+        </div>
+
+
+        <div className="btn-total">
+          <button onClick={() => {
+            console.log("UMBRAL NAME: " + umbralName)
+            ManageMetrics(user, selectedProject, umbralName)
+          }
+          }>
+            Calcular Metricas
+          </button>
+        </div>
+
+
+        <div>
+          <Button onClick={calculateTotal} variant="contained">
+            Calcular
+          </Button>
+        </div>
+
+
+      </form >
       <div className="total-sum">
         <p>
-          Total:<span>{sum}</span>
+          Total:<span>{total}</span>
         </p>
       </div>
       {
